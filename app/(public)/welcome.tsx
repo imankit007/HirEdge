404b4ac416932213f260bc37b06a071cbeb3b7b7
AmritSingh1502 <113255930+AmritSingh1502@users.noreply.github.com>
2023-10-15 08:08:58 +0530

    welcome page

--- app/(public)/welcome.tsx
+++ app/(public)/welcome.tsx
@@ -1,14 +1,33 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+// import {useNavigation} from '@react-navigation/native';  
 
+const WelcomePage = ({ }) => {
+  // const navigation = useNavigation();
 
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const Welcome = () => {
-    return (
-        <View>
-            <Text>Welcome</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Placement Management!</Text>
+      {/* <Button 
+        title="Login"
+        onPress={() => navigation.navigate('StartPage')} 
+      /> */}
+    </View>
+  );
 }
 
-export default Welcome;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center', 
+    justifyContent: 'center',
+    padding: 20
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20
+  }
+});
+
+export default WelcomePage;
