b994782b888c700254cc282f4f2a51388e465318
Ankit <50232341+imankit007@users.noreply.github.com>
2023-12-11 23:18:17 +0530

    Update axios.ts

--- utils/axios.ts
+++ utils/axios.ts
@@ -3,17 +3,17 @@ import axios from "axios";
 
 //replace the baseURL with IP of your machine
 // ipconfig - to find the ip address of machine
-
+const baseURL = 'http://192.168.37.181:5000';
 
 const instance = axios.create({
-    baseURL:'http://192.168.191.131:5000',
+    baseURL:baseURL,
     responseType: 'json'
 
 })
 
 
 export const axiosPrivate = axios.create({
-    baseURL: 'http://192.168.191.131:5000',
+    baseURL: baseURL,
     withCredentials: true,
     headers:{
         "Content-Type": 'application/json',
