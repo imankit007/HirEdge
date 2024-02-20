6855a1ff4367c44482fbff2327afdfc50f58fcdf
Ankit <50232341+imankit007@users.noreply.github.com>
2024-02-20 23:49:41 +0530

    Update axios.ts

--- app/utils/axios.ts
+++ app/utils/axios.ts
@@ -3,11 +3,7 @@ import axios from "axios";
 
 //replace the baseURL with IP of your machine
 // ipconfig - to find the ip address of machine
-<<<<<<< Updated upstream
 const baseURL = 'http://192.168.40.68:5000';
-=======
-const baseURL = 'http://192.168.173.131:5000';
->>>>>>> Stashed changes
 
 
 const instance = axios.create({
